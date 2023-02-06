72efbff25255b60850371649afda2d2c94c17f41
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-06 22:26:45 +0100

    refactor test file to be used by mocha

--- 02_course code/32_the_basic_testing/index.test.js
+++ 02_course code/32_the_basic_testing/index.test.js
@@ -9,16 +9,16 @@ const { forEach, map } = require('./index.js');
 // 1. move test from global scope, can repeat variables names
 // 2. try catch bloke no hang up app
 // 3. can pass description to distinguish tests
-const test = (description, fn) => {
-	console.log('----', description);
-	try {
-		fn();
-	} catch (error) {
-		console.log(error.message);
-	}
-};
+// const test = (description, fn) => {
+// 	console.log('----', description);
+// 	try {
+// 		fn();
+// 	} catch (error) {
+// 		console.log(error.message);
+// 	}
+// };
 
-test('The forEach function', () => {
+it('The forEach function', () => {
 	let sum = 0;
 	forEach([1, 2, 3], (value) => {
 		sum += value;
@@ -28,7 +28,7 @@ test('The forEach function', () => {
 	assert.strictEqual(sum, 6, 'Expected summing array');
 });
 
-test('The map function', () => {
+it('The map function', () => {
 	const result = map([1, 2, 3], (value) => {
 		return value * 2;
 	});
@@ -36,5 +36,5 @@ test('The map function', () => {
 	// assert.strictEqual(result[0], 2);
 	// assert.strictEqual(result[1], 4);
 	// assert.strictEqual(result[2], 7);
-	assert.deepEqual(result, [2, 4, 7]);
+	assert.deepEqual(result, [2, 4, 6]);
 });
