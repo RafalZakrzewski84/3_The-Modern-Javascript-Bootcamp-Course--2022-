053bb7c6ca1e942c92e24c8f565e08636563c8a8
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-07 19:36:21 +0100

    holding up test assertion by async and waitFor fnc

--- 02_course code/32_the_basic_testing/testing_movie_app/test/autocomplete.test.js
+++ 02_course code/32_the_basic_testing/testing_movie_app/test/autocomplete.test.js
@@ -1,3 +1,19 @@
+const waitFor = (selector) => {
+	return new Promise((resolve, reject) => {
+		const interval = setInterval(() => {
+			if (document.querySelector(selector)) {
+				clearInterval(interval);
+				clearTimeout(timeOut);
+				resolve();
+			}
+		}, 30);
+		const timeOut = setTimeout(() => {
+			clearInterval(interval);
+			reject();
+		}, 2000);
+	});
+};
+
 beforeEach(() => {
 	document.querySelector('#target').innerHTML = '';
 	createAutoComplete({
@@ -20,12 +36,15 @@ it('dropdown closed on start', () => {
 	expect(dropdown.className).not.to.include('is-active');
 });
 
-it('after searching, dropdown opens up', () => {
+it('after searching, dropdown opens up', async () => {
 	//simulating searching
 	const input = document.querySelector('input');
 	input.value = 'Avengers';
 	input.dispatchEvent(new Event('input'));
 
+	//test will fail because searching use debounce fnc
+	//need to hold on assertion by wait for function
+	await waitFor('.dropdown-item');
 	const dropdown = document.querySelector('.dropdown');
 	expect(dropdown.className).to.include('is-active');
 });
