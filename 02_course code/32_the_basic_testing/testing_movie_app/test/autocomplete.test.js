334729910e27b63f5488c93a52ad66df3f620326
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-07 18:45:14 +0100

    displaying autocomplete

--- 02_course code/32_the_basic_testing/testing_movie_app/test/autocomplete.test.js
+++ 02_course code/32_the_basic_testing/testing_movie_app/test/autocomplete.test.js
@@ -1,1 +1,15 @@
-it('works', () => {});
+it('works', () => {
+	createAutoComplete({
+		root: document.querySelector('#target'),
+		fetchData() {
+			return [
+				{ Title: 'Avengers' },
+				{ Title: 'Not Avengers' },
+				{ Title: 'Some other movies' },
+			];
+		},
+		renderOption(movie) {
+			return movie.Title;
+		},
+	});
+});
