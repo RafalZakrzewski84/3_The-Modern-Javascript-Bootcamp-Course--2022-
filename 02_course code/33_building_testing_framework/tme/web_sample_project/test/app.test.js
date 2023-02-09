0bb09150b65b9f14bc805ffbecbb073637fe96c1
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-02-10 00:31:57 +0100

    adding testing cases when input has value after loading scripts

--- 02_course code/33_building_testing_framework/tme/web_sample_project/test/app.test.js
+++ 02_course code/33_building_testing_framework/tme/web_sample_project/test/app.test.js
@@ -4,6 +4,31 @@ it('has a text input', async () => {
   const dom = await render('index.html');
 
   const input = dom.window.document.querySelector('input');
-
   assert(input);
+});
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'alskdjf@aslkdjf.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h3 = dom.window.document.querySelector('h3');
+  assert.strictEqual(h3.innerText, 'email is valid');
+});
+
+it('shows a error message with a invalid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'alskdjfaslkdjf.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h3 = dom.window.document.querySelector('h3');
+  assert.strictEqual(h3.innerText, 'email is invalid');
 });
